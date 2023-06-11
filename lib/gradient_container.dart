c3aa419f4e6eee8d32b5fe983242abccce7a74bf
GauravRoy-20 <gauravroy20.gr@gmail.com>
2023-06-11 22:56:43 +0530

    first commit

---
+++ lib/gradient_container.dart
@@ -0,0 +1,24 @@
+import "package:flutter/material.dart";
+import "package:udemy_flutter/styled_text.dart";
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Color.fromARGB(255, 72, 39, 127),
+            Color.fromARGB(255, 85, 4, 117)
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+      ),
+      child: const Center(
+        child: StyledText(),
+      ),
+    );
+  }
+}
